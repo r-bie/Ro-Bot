358e9bdf3e64bf337d31b3bf2aa5a1bf07f42746
unknown <robiet6@gmail.com>
2022-12-28 05:47:34 +0800

    merge RP Bot commands

---
+++ commands/tools/doxx.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const w3func = require('../../web3');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('doxx')
+        .setDescription('Get the SKILL balance of the user address.')
+        .addStringOption(option => option.setName('owner').setDescription('Metamask Address')),
+    async execute(interaction, client) {
+        const message = await interaction.deferReply({
+            fetchReply: true
+        });
+
+        var author = interaction.user.id;
+        console.log(`${author} used doxx command.`);
+        
+        var address = interaction.options.getString('owner');
+        const balance = await w3func.getBalanceOf('bnb', address);
+        
+        const newMessage = `Balance of ${address} is ${balance}`;
+        await interaction.editReply({
+            content: newMessage
+        });
+    }
+}
---
+++ commands/tools/watch.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const w3func = require('../../web3');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('watch')
+        .setDescription('Start watching the Rewards Pool.'),
+    async execute(interaction, client) {
+        const message = await interaction.deferReply({
+            fetchReply: true
+        });
+
+        // logging into dyno-logs text channel
+        var author = interaction.user.id;
+        console.log(`${author} used watch command.`);
+        
+        const newMessage = `Now watching the Rewards Pool.`;
+        w3func.getRewardsPoolBalance('bnb', client);
+        await interaction.editReply({
+            content: newMessage
+        });
+    }
+}
